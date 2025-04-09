2dd6ee88f346ddceac36ac6ccee9024b8084030b
DataDev <104727812+KinexDev@users.noreply.github.com>
2025-04-09 23:25:23 +0100

    finished

---
+++ build.lua
@@ -0,0 +1,18 @@
+return {
+    -- name of the program
+    name = "snake",
+    -- starting point of the program, the main script.
+    main = "src/main.luau",
+    -- additional scripts to compile because we are using them via require
+    scripts = { 
+        "src/game.luau", 
+        "src/player.luau", 
+        "src/raylib-utils.luau",
+        "src/raylib.luau",
+        "src/rendering.luau" 
+    },
+    -- files that are copied over after compilation
+    dependencies = {
+        "raylib-luau.dll"
+    }
+}
